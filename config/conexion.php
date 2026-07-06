d5f1121a1bdb0aa3946e356724c473df41fb15d7
Jonathan Vaccaro <jonathan.vaccaroaco@ug.edu.ec>
2026-07-06 18:15:57 -0500

    Agregar soporte SSL para conexion MySQL en despliegue

--- config/conexion.php
+++ config/conexion.php
@@ -22,16 +22,36 @@ class Conexion
         $usuario = self::obtenerVariable("DB_USER", "root");
         $clave = self::obtenerVariable("DB_PASS", "");
         $puerto = (int) self::obtenerVariable("DB_PORT", "3307");
+        $sslCa = self::obtenerVariable("DB_SSL_CA", "");
 
-        $conexion = new mysqli(
-            $host,
-            $usuario,
-            $clave,
-            $baseDatos,
-            $puerto
-        );
+        $conexion = mysqli_init();
 
-        if ($conexion->connect_error) {
+        if (!$conexion) {
+            die("Error al inicializar la conexión MySQL.");
+        }
+
+        if ($sslCa !== "" && file_exists($sslCa)) {
+            $conexion->ssl_set(null, null, $sslCa, null, null);
+            $conectado = $conexion->real_connect(
+                $host,
+                $usuario,
+                $clave,
+                $baseDatos,
+                $puerto,
+                null,
+                MYSQLI_CLIENT_SSL
+            );
+        } else {
+            $conectado = $conexion->real_connect(
+                $host,
+                $usuario,
+                $clave,
+                $baseDatos,
+                $puerto
+            );
+        }
+
+        if (!$conectado) {
             die("Error de conexión a la base de datos: " . $conexion->connect_error);
         }
 
